8aab3c0592596b158416ead2a39bb5bca7b07625
Elisa <elisa2009ferrara@gmail.com>
2025-03-07 16:24:39 +0100

    aggiunta schermata registrazione

--- src/main/java/com/uninadelivery/model/dbconnection/DBConnection.java
+++ src/main/java/com/uninadelivery/model/dbconnection/DBConnection.java
@@ -10,7 +10,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/UninaDelivery";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "1234";
+    private static final String PASSWORD = "postgres";
 
     private DBConnection() {}
 
