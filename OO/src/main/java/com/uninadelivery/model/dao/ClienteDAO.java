e5d7be76716865ddc360dd81d5e11f02637880a8
Fragux03 <fragux03@gmail.com>
2025-03-29 14:09:29 +0100

    fix

--- OO/src/main/java/com/uninadelivery/model/dao/ClienteDAO.java
+++ OO/src/main/java/com/uninadelivery/model/dao/ClienteDAO.java
@@ -1,14 +1,5 @@
 package com.uninadelivery.model.dao;
 
-import com.uninadelivery.model.dbconnection.DBConnection;
-import java.sql.*;
-import java.util.logging.Logger;
-
 public class ClienteDAO {
-    private static final Logger LOGGER = Logger.getLogger(ClienteDAO.class.getName());
-    private final Connection conn;
 
-    public ClienteDAO() throws SQLException {
-        this.conn = DBConnection.getDBconnection().getConnection();
-    }
 }
